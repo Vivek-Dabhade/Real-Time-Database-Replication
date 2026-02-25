7992fa65e06b18356e9a671da40568bcf0614a8e
Vivek Ganpat Dabhade <vivekdabhade7779@gmail.com>
2026-02-25 09:49:48 +0530

    Resolved errors and scripts updated. The data is fetched in mariadb. Everything is working fine

--- app/schema.py
+++ app/schema.py
@@ -6,27 +6,30 @@
 
 import sqlalchemy
 
-pg_engine = sqlalchemy.create_engine(
-    "postgresql+psycopg2://root:root@portgres:5432/crypto", echo=True
-)
-mysql_engine = sqlalchemy.create_engine(
-    "mysql+pymysql://root:root@mysql:3306/myapp", echo=True
+# pg_engine = sqlalchemy.create_engine(
+# "postgresql+psycopg2://root:root@portgres:5432/crypto", echo=True
+# )
+mariadb_engine = sqlalchemy.create_engine(
+    "mysql+pymysql://root:root@mariadb:3306/mariadb_db", echo=True
 )
 
-pg_connection = pg_engine.connet()
-mysql_connection = mysql_engine.connect()
+# pg_connection = pg_engine.connect()
+mariadb_connection = mariadb_engine.connect()
 
-metadata = sqlalchemy.Metadata()
+metadata = sqlalchemy.MetaData()
 
 user_table = sqlalchemy.Table(
     "crypto",
+    metadata,
     sqlalchemy.Column("id", sqlalchemy.Integer, primary_key=True),
-    sqlalchemy.Coulmn("price", sqlalchemy.Float),
+    sqlalchemy.Column("price", sqlalchemy.Float),
     sqlalchemy.Column("quantity", sqlalchemy.Float),
 )
-metadata.create_all(mysql_engine)
+metadata.create_all(mariadb_engine)
 
 
 def sql_insert_data(price: float, quantity: float) -> None:
     query = user_table.insert().values(price=price, quantity=quantity)
-    mysql_connection.excute(query)
+    with mariadb_engine.connect() as conn:
+        conn.execute(query)
+        conn.commit()
